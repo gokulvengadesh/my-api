f105ee819d0b7bacfd456e319a26d8ae6b16a80a
Gokul Vengadesh <gokulvengadeshk@gmail.com>
2025-07-11 10:18:40 +0530

    Added /api/users route with static user data

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const app = express();
 const PORT = process.env.PORT || 10000;
 
 const users = [
-  { id: 1, name: 'gokul', age: 25 },
-  { id: 2, name: 'Bob', age: 30 },
-  { id: 3, name: 'Charlie', age: 28 }
+  { image: 'https://via.placeholder.com/100', id: 1, name: 'gokul', age: 25 },
+  { image: 'https://via.placeholder.com/100', id: 2, name: 'Bob', age: 30 },
+  { image: 'https://via.placeholder.com/100', id: 3, name: 'Charlie', age: 28 }
 ];
 
 app.get('/api/users', (req, res) => {
